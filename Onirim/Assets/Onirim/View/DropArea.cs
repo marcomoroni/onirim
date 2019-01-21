d4e5cfb8b5b99373219fdc962cafab68b4e57547
Marco Moroni <marcomoroni394@gmail.com>
2019-01-21 18:05:51 +0000

    Create drop area

---
+++ Onirim/Assets/Onirim/View/DropArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DropArea : MonoBehaviour
+{
+	private BoxCollider2D _boxCollider2D;
+
+	private void Start()
+	{
+		_boxCollider2D = GetComponent<BoxCollider2D>();
+	}
+}
